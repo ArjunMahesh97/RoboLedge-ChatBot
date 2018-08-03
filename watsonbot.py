686d8dd53100c40b7aaf382769f7002b1c210005
ArjunMahesh97 <arjunmahesh97@gmail.com>
2018-08-03 10:32:13 +0530

    Update watsonbot.py

--- watsonbot.py
+++ watsonbot.py
@@ -1,15 +1,27 @@
 from telegram.ext import Updater, MessageHandler, CommandHandler, Filters
 from watson_developer_cloud import ConversationV1
 import json
+import sqlite3
+
 
 context = None
 
+conn = sqlite3.connect('budget.db', check_same_thread=False, timeout=10)
+c=conn.cursor()
+
+
+#query = "SELECT date FROM expenditure1 WHERE userID = 'arjun' and amount=500"
+#c.execute(query)
+#r=c.fetchall()
+#r1=r[1][1]
+    
 
 # Define a few command handlers. These usually take the two arguments bot and
 # update. Error handlers also receive the raised TelegramError object in error.
 def start(bot, update):
     print('Received /start command')
-    update.message.reply_text('Hola!\nI\'m here to help you save some money.\nDo you want to get started? :) ')
+    update.message.reply_text('Hola!\nI\'m here to help you save some money.\nPlease enter your name to continue :) ')
+    #update.message.reply_text(r[0][0])
 
 
 def help(bot, update):
@@ -37,8 +49,92 @@ def message(bot, update):
     resp = ''
     for text in response['output']['text']:
         resp += text
+    #update.message.reply_text(resp)
+	
+    if len(response['entities'])>0:    	
+	entity = response['entities'][0]['entity']
+ 	value = response['entities'][0]['value']
+    
+    # handle no intents
+    if len(response['intents'])>0:
+    	intent=response['intents'][0]['intent']
+        print(intent)
+    
 
-    update.message.reply_text(resp)
+    context = response['context']
+    # build response
+
+    #print(response['entities'][4]['metadata']['numeric_value'])
+	 	
+    if intent == 'enter_name':
+    	global name
+    	name = response['input']['text']
+    	print(name)
+    	update.message.reply_text(response['output']['text'][0])
+    	
+    	
+    if intent == 'Set_budget':
+    	print(name)
+        budg=response['entities'][2]['metadata']['numeric_value']
+        date = response['entities'][0]['value']
+        #print(date)
+        #print(response['entities'][4]['metadata']['numeric_value'])
+	update.message.reply_text(response['output']['text'][0])
+        query = 'INSERT INTO expenditure1 (userID,date,budget) VALUES("%s","%s",%d)'%(name,date,budg)
+	print(query)
+	try:
+		c.execute(query)
+		conn.commit()
+	except Exception as e:
+		print(e)
+	#c.execute("SELECT * FROM expenditure1")
+	#r=c.fetchall()	
+	#print(r)
+	#print(response['entities'][4]['metadata']['numeric_value'])
+
+
+    if intent == 'Spent_money_on':
+	spent=response['entities'][1]['metadata']['numeric_value']
+	item=response['context']['item']
+	#print(response['entities'][1]['metadata']['numeric_value'])
+	update.message.reply_text(response['output']['text'][0])
+	query= 'UPDATE expenditure1 SET amount=%d , item="%s" WHERE userID = "%s" '%(spent,item,name)
+	query2= 'UPDATE expenditure1 SET budget=budget-%d WHERE userID = "%s"'%(spent , name)
+	#query3='SELECT budget FROM expenditure1 WHERE userID="%s"'%(name)
+	print(query2)
+	try:
+		#print(c.execute(query))		
+		c.execute(query)
+		c.execute(query2)
+		#c.execute(query3)
+		conn.commit()
+		#r=c.fetchall()
+		#print(r)
+		
+	except Exception as e:
+		print(e)
+	
+
+
+    if intent == 'Remaining_budget':
+	outp=response['output']['text'][0]
+	query='SELECT budget FROM expenditure1 WHERE userID="%s"'%(name)
+	print(query)
+	try:
+		#print(c.execute(query))		
+		c.execute(query)
+		conn.commit()
+		r=c.fetchall()
+		print(r[0][0])
+		budg=str(r[0][0]) 
+		if r[0][0] > 0:
+			update.message.reply_text(outp+" "+budg)
+		else:
+			update.message.reply_text("Oops! You have already exceeded your budget")
+	except Exception as e:
+		print(e)
+	
+		
 
 
 def main():
@@ -60,7 +156,8 @@ def main():
 
     # Block until the user presses Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
+    # start_polling() is non-blockin
+    # g and will stop the bot gracefully.
     updater.idle()
 
 
